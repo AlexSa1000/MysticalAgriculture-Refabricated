6f4e72be5a4299c38727a53a57f65e3fb14f17cd
AlexSa1000 <56562013+AlexSa1000@users.noreply.github.com>
2021-05-28 00:34:37 +0100

    Made essence farmland not turn to dirt when trampled or dried

--- src/main/java/com/alex/mysticalagriculture/blocks/InfusedFarmlandBlock.java
+++ src/main/java/com/alex/mysticalagriculture/blocks/InfusedFarmlandBlock.java
@@ -7,12 +7,17 @@ import net.minecraft.block.Blocks;
 import net.minecraft.block.FarmlandBlock;
 import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.enchantment.Enchantments;
+import net.minecraft.entity.Entity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.loot.context.LootContext;
 import net.minecraft.loot.context.LootContextParameters;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class InfusedFarmlandBlock extends FarmlandBlock implements Colored {
     public static final List<InfusedFarmlandBlock> FARMLANDS = new ArrayList<>();
@@ -23,6 +28,24 @@ public class InfusedFarmlandBlock extends FarmlandBlock implements Colored {
         this.tier = tier;
 
         FARMLANDS.add(this);
+    }
+
+    @Override
+    public void onLandedUpon(World world, BlockPos pos, Entity entity, float distance) {
+        entity.handleFallDamage(distance, 1.0F);
+    }
+
+    @Override
+    public void randomTick(BlockState state, ServerWorld world, BlockPos pos, Random random) {
+        int moisture = state.get(MOISTURE);
+
+        if (!isWaterNearby(world, pos) && !world.hasRain(pos.up())) {
+            if (moisture > 0) {
+                world.setBlockState(pos, state.with(MOISTURE, moisture - 1), 2);
+            }
+        } else if (moisture < 7) {
+            world.setBlockState(pos, state.with(MOISTURE, 7), 2);
+        }
     }
 
     @Override
