6729a62fbe1788c9a14e1f6baa59ac55ce9ce0e5
AlexSa1000 <56562013+AlexSa1000@users.noreply.github.com>
2021-05-28 00:33:03 +0100

    Added CropTierProvider

---
+++ src/main/java/com/alex/mysticalagriculture/api/crop/CropTierProvider.java
@@ -0,0 +1,9 @@
+package com.alex.mysticalagriculture.api.crop;
+
+public interface CropTierProvider {
+    /**
+     * Gets the crop tier from this object
+     * @return the crop tier
+     */
+    CropTier getTier();
+}
